fbd5b29d97230950b63f9d32aa9ba6fdd92e1019
Gautam <sahnigautam69@gmail.com>
2021-04-16 12:49:26 +0530

    [ADD] xlsx report done

--- Custom/test_Module/report/student_card_xlsx.py
+++ Custom/test_Module/report/student_card_xlsx.py
@@ -7,15 +7,20 @@ class PartnerXlsx(models.AbstractModel):
 
     def generate_xlsx_report(self, workbook, data, lines):
         print("lines", lines)
-        format = workbook.add_format({'font_size':14, 'align': 'vcenter', 'bold': True})
+        format = workbook.add_format({'font_size': 14, 'align': 'vcenter', 'bold': True})
+        format1 = workbook.add_format({'font_size': 14, 'align': 'vcenter'})
         sheet = workbook.add_worksheet('Student Card')
 
         # bold = workbook.add_format({'bold': True})
 
         # sheet.writer(1,1,lines.student_name,format)
+        sheet.write(0, 0, 'Name', format)
+        sheet.write(0, 1, 'Age', format)
+        sheet.write(0, 2, 'Gender', format)
+        sheet.write(0, 3, 'Description', format)
 
-        for i in range(10):
-            sheet.write(i, 0, 'Name', format)
-            sheet.write(i,1, 'Age', format)
-            sheet.write(i,2, 'Gender', format)
-            sheet.write(i,3, 'Description', format)
+        for i, line in enumerate(lines):
+            sheet.write(i+1, 0, line.name, format1)
+            sheet.write(i+1, 1, line.age, format1)
+            sheet.write(i+1, 2, line.gender, format1)
+            sheet.write(i+1, 3, line.note, format1)
